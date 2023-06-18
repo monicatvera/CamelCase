181685465d3447f4721f9566511a2a9bff6b4d9c
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-06-18 13:24:50 +0100

    ✅ implemented the function

--- src/core/camelCase.ts
+++ src/core/camelCase.ts
@@ -1,3 +1,4 @@
-export function toCamelCase(text: string) {
-	return '';
+export function toCamelCase(text: string) :string {
+	const words = text.split(/[ _-]/);
+	return words.map((word) => word.charAt(0).toUpperCase() + word.substr(1)).join('');
 }
